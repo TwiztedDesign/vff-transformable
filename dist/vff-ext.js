0bc5c73f5cdc60edeb61fa2a3090ad8c1ffcf90e
dok <dumansky@gmail.com>
2019-01-05 22:27:19 +0200

    transformable

--- dist/vff-ext.js
+++ dist/vff-ext.js
@@ -179,17 +179,9 @@ var VffTransformable = function (_HTMLElement) {
 
             // update the element's style
 
-            target.style.width = event.rect.width + 'px';
-            target.style.height = event.rect.height + 'px';
-
             // translate when resizing from top or left edges
-            x += event.deltaRect.left;
-            y += event.deltaRect.top;
 
             target.style.webkitTransform = target.style.transform = 'translate(' + x + 'px,' + y + 'px)';
-
-            target.setAttribute('data-x', x);
-            target.setAttribute('data-y', y);
         }
     }]);
 
--- src/transformable.js
+++ src/transformable.js
@@ -73,19 +73,10 @@ export default class VffTransformable extends HTMLElement {
 
         // update the element's style
 
-        target.style.width  = event.rect.width + 'px';
-        target.style.height = event.rect.height + 'px';
-
         // translate when resizing from top or left edges
-        x += event.deltaRect.left;
-        y += event.deltaRect.top;
 
         target.style.webkitTransform = target.style.transform =
             'translate(' + x + 'px,' + y + 'px)';
-
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-
     }
 
     disconnectedCallback() {
